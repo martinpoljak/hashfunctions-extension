70993ab767f12c6a3c889e0f2147cf1bc94b1ecf
Martin Kozák <martinkozak@martinkozak.net>
2012-01-28 23:00:36 +0100

    pointer to string should be unsigned char, not signed

--- ext/generalhashfunctions_ext/AP_GeneralHashFunctions.c
+++ ext/generalhashfunctions_ext/AP_GeneralHashFunctions.c
@@ -1,12 +1,12 @@
 #include "AP_GeneralHashFunctions.h"
 
-unsigned int RSHash(char* str, unsigned int len)
+unsigned int RSHash(unsigned char* str, unsigned int len)
 {
    unsigned int b    = 378551;
    unsigned int a    = 63689;
    unsigned int hash = 0;
    unsigned int i    = 0;
-
+   
    for(i = 0; i < len; str++, i++)
    {
       hash = hash * a + (*str);
@@ -18,7 +18,7 @@ unsigned int RSHash(char* str, unsigned int len)
 /* End Of RS Hash Function */
 
 
-unsigned int JSHash(char* str, unsigned int len)
+unsigned int JSHash(unsigned char* str, unsigned int len)
 {
    unsigned int hash = 1315423911;
    unsigned int i    = 0;
@@ -33,7 +33,7 @@ unsigned int JSHash(char* str, unsigned int len)
 /* End Of JS Hash Function */
 
 
-unsigned int PJWHash(char* str, unsigned int len)
+unsigned int PJWHash(unsigned char* str, unsigned int len)
 {
    const unsigned int BitsInUnsignedInt = (unsigned int)(sizeof(unsigned int) * 8);
    const unsigned int ThreeQuarters     = (unsigned int)((BitsInUnsignedInt  * 3) / 4);
@@ -58,7 +58,7 @@ unsigned int PJWHash(char* str, unsigned int len)
 /* End Of  P. J. Weinberger Hash Function */
 
 
-unsigned int ELFHash(char* str, unsigned int len)
+unsigned int ELFHash(unsigned char* str, unsigned int len)
 {
    unsigned int hash = 0;
    unsigned int x    = 0;
@@ -79,7 +79,7 @@ unsigned int ELFHash(char* str, unsigned int len)
 /* End Of ELF Hash Function */
 
 
-unsigned int BKDRHash(char* str, unsigned int len)
+unsigned int BKDRHash(unsigned char* str, unsigned int len)
 {
    unsigned int seed = 131; /* 31 131 1313 13131 131313 etc.. */
    unsigned int hash = 0;
@@ -95,7 +95,7 @@ unsigned int BKDRHash(char* str, unsigned int len)
 /* End Of BKDR Hash Function */
 
 
-unsigned int SDBMHash(char* str, unsigned int len)
+unsigned int SDBMHash(unsigned char* str, unsigned int len)
 {
    unsigned int hash = 0;
    unsigned int i    = 0;
@@ -110,7 +110,7 @@ unsigned int SDBMHash(char* str, unsigned int len)
 /* End Of SDBM Hash Function */
 
 
-unsigned int DJBHash(char* str, unsigned int len)
+unsigned int DJBHash(unsigned char* str, unsigned int len)
 {
    unsigned int hash = 5381;
    unsigned int i    = 0;
@@ -125,7 +125,7 @@ unsigned int DJBHash(char* str, unsigned int len)
 /* End Of DJB Hash Function */
 
 
-unsigned int DEKHash(char* str, unsigned int len)
+unsigned int DEKHash(unsigned char* str, unsigned int len)
 {
    unsigned int hash = len;
    unsigned int i    = 0;
@@ -139,7 +139,7 @@ unsigned int DEKHash(char* str, unsigned int len)
 /* End Of DEK Hash Function */
 
 
-unsigned int BPHash(char* str, unsigned int len)
+unsigned int BPHash(unsigned char* str, unsigned int len)
 {
    unsigned int hash = 0;
    unsigned int i    = 0;
@@ -153,7 +153,7 @@ unsigned int BPHash(char* str, unsigned int len)
 /* End Of BP Hash Function */
 
 
-unsigned int FNVHash(char* str, unsigned int len)
+unsigned int FNVHash(unsigned char* str, unsigned int len)
 {
    const unsigned int fnv_prime = 0x811C9DC5;
    unsigned int hash      = 0;
@@ -170,7 +170,7 @@ unsigned int FNVHash(char* str, unsigned int len)
 /* End Of FNV Hash Function */
 
 
-unsigned int APHash(char* str, unsigned int len)
+unsigned int APHash(unsigned char* str, unsigned int len)
 {
    unsigned int hash = 0xAAAAAAAA;
    unsigned int i    = 0;
--- ext/generalhashfunctions_ext/AP_GeneralHashFunctions.h
+++ ext/generalhashfunctions_ext/AP_GeneralHashFunctions.h
@@ -25,20 +25,20 @@
 #include <stdio.h>
 
 
-typedef unsigned int (*hash_function)(char*, unsigned int len);
+typedef unsigned int (*hash_function)(unsigned char*, unsigned int len);
 
 
-unsigned int RSHash  (char* str, unsigned int len);
-unsigned int JSHash  (char* str, unsigned int len);
-unsigned int PJWHash (char* str, unsigned int len);
-unsigned int ELFHash (char* str, unsigned int len);
-unsigned int BKDRHash(char* str, unsigned int len);
-unsigned int SDBMHash(char* str, unsigned int len);
-unsigned int DJBHash (char* str, unsigned int len);
-unsigned int DEKHash (char* str, unsigned int len);
-unsigned int BPHash  (char* str, unsigned int len);
-unsigned int FNVHash (char* str, unsigned int len);
-unsigned int APHash  (char* str, unsigned int len);
+unsigned int RSHash  (unsigned char* str, unsigned int len);
+unsigned int JSHash  (unsigned char* str, unsigned int len);
+unsigned int PJWHash (unsigned char* str, unsigned int len);
+unsigned int ELFHash (unsigned char* str, unsigned int len);
+unsigned int BKDRHash(unsigned char* str, unsigned int len);
+unsigned int SDBMHash(unsigned char* str, unsigned int len);
+unsigned int DJBHash (unsigned char* str, unsigned int len);
+unsigned int DEKHash (unsigned char* str, unsigned int len);
+unsigned int BPHash  (unsigned char* str, unsigned int len);
+unsigned int FNVHash (unsigned char* str, unsigned int len);
+unsigned int APHash  (unsigned char* str, unsigned int len);
 
 
 #endif
